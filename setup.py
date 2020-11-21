b2ff60de5bdc8cb953dc34c311f090c414cde3ef
DaloroAT <43877180+DaloroAT@users.noreply.github.com>
2020-11-21 13:06:03 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="python_utils",
+    packages=find_packages(),
+    version="0.1.0",
+    description="A set of useful scripts",
+    author="Aleksei Tarasov"
+)
